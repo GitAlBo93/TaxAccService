de4931a7ac99ea023919c77d9b954f320203fc7f
GitAlBo93 <a.l.e.k.s.93@mail.ru>
2026-05-09 21:03:10 +0500

    refactor: Измен MainLayout в части Outlet. Обновлены UI страниц LoginPage и RegistrationPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,6 @@
 import { Button, InputField, Link, T } from '@admiral-ds/react-ui';
 import { RouteEnum } from '../../app/constants';
-import { FormContainer, InputContainer, RegistrationLinkContainer } from './LoginPage.styles';
+import { FormContainer, InputContainer, LoginPageContainer, RegistrationLinkContainer } from './LoginPage.styles';
 import { Controller } from 'react-hook-form';
 import { useLoginForm } from './useLoginForm';
 
@@ -8,48 +8,50 @@ export const LoginPage = () => {
   const { handleSubmit, handleLogin, control, errors } = useLoginForm();
 
   return (
-    <FormContainer onSubmit={handleSubmit(handleLogin)}>
-      <T font="Main/XL" color="Neutral/Neutral 90">
-        Добро пожаловать!
-      </T>
-      <InputContainer>
-        <Controller
-          name="username"
-          control={control}
-          render={({ field }) => (
-            <InputField
-              {...field}
-              label="Имя пользователя"
-              placeholder="Имя"
-              status={errors.username ? 'error' : undefined}
-              extraText={errors.username?.message}
-            />
-          )}
-        />
-        <Controller
-          name="password"
-          control={control}
-          render={({ field }) => (
-            <InputField
-              {...field}
-              type="password"
-              label="Пароль"
-              placeholder="Пароль"
-              status={errors.password ? 'error' : undefined}
-              extraText={errors.password?.message}
-            />
-          )}
-        />
-        <RegistrationLinkContainer>
-          <T font="Additional/XS">Ещё нет аккаунта?</T>
-          <Link appearance="primary" dimension="s" href={RouteEnum.registration}>
-            Зарегистрироваться
-          </Link>
-        </RegistrationLinkContainer>
-      </InputContainer>
-      <Button dimension="xl" appearance="primary" type="submit">
-        Войти
-      </Button>
-    </FormContainer>
+    <LoginPageContainer>
+      <FormContainer onSubmit={handleSubmit(handleLogin)}>
+        <T font="Main/XL" color="Neutral/Neutral 90">
+          Добро пожаловать!
+        </T>
+        <InputContainer>
+          <Controller
+            name="username"
+            control={control}
+            render={({ field }) => (
+              <InputField
+                {...field}
+                label="Имя пользователя"
+                placeholder="Имя"
+                status={errors.username ? 'error' : undefined}
+                extraText={errors.username?.message}
+              />
+            )}
+          />
+          <Controller
+            name="password"
+            control={control}
+            render={({ field }) => (
+              <InputField
+                {...field}
+                type="password"
+                label="Пароль"
+                placeholder="Пароль"
+                status={errors.password ? 'error' : undefined}
+                extraText={errors.password?.message}
+              />
+            )}
+          />
+          <RegistrationLinkContainer>
+            <T font="Additional/XS">Ещё нет аккаунта?</T>
+            <Link appearance="primary" dimension="s" href={RouteEnum.registration}>
+              Зарегистрироваться
+            </Link>
+          </RegistrationLinkContainer>
+        </InputContainer>
+        <Button dimension="xl" appearance="primary" type="submit">
+          Войти
+        </Button>
+      </FormContainer>
+    </LoginPageContainer>
   );
 };
